c5f68543f7b1ffadffd6642e1471bc14b7f00331
Esau Matias <esaufreirematias@gmail.com>
2021-11-20 14:03:37 -0300

    Requisito 08 - Adiciona produtos a partir da tela de listagem de produtos

--- src/componets/BtnCart.js
+++ src/componets/BtnCart.js
@@ -2,11 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class BtnCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sum: 0,
+    };
+    this.getSoma = this.getSoma.bind(this);
+  }
+
+  componentDidMount() {
+    this.getSoma();
+  }
+
+  getSoma() {
+    const soma = JSON.parse(localStorage.getItem('soma'));
+    this.setState((prevState) => ({
+      sum: prevState.sum + soma,
+    }));
+  }
+
   render() {
+    const { sum } = this.state;
     return (
       <div>
         <Link to="/shopping-cart" data-testid="shopping-cart-button">
           <i className="fas fa-shopping-cart" />
+          <div data-testid="shopping-cart-product-quantity">{ sum }</div>
         </Link>
       </div>
     );
